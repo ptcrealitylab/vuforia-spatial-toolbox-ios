c4583befedcd7c8d7177cee0a81d5b94248b1f18
Valentin Heun <heun@mit.edu>
2015-11-24 18:13:47 -0500

    Fix the orientation problem on older iOS devices

--- src/main.mm
+++ src/main.mm
@@ -12,6 +12,7 @@ int main() {
     settings.enableAntiAliasing = false; // enables anti-aliasing which smooths out graphics on the screen.
     settings.numOfAntiAliasingSamples = 0; // number of samples used for anti-aliasing.
     settings.enableHardwareOrientation = true; // enables native view orientation.
+    settings.setupOrientation = OF_ORIENTATION_90_RIGHT;
     settings.enableHardwareOrientationAnimation = false; // enables native orientation changes to be animated.
     settings.glesVersion = OFXIOS_RENDERER_ES2; // type of renderer to use, ES1, ES2, ES3
     settings.windowMode = OF_FULLSCREEN;
