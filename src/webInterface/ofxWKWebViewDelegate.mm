bf21e4273941aea9f5a698be2c0dc7b2a794a9d1
Valentin Heun <heun@mit.edu>
2017-05-22 00:10:49 -0400

    License

--- src/webInterface/ofxWKWebViewDelegate.mm
+++ src/webInterface/ofxWKWebViewDelegate.mm
@@ -1,10 +1,48 @@
-//
-//  ofxWKWebViewDelegate.cpp
-//  RealityEditor
-//
-//  Created by Fluid Interfaces Group on 2/22/16.
-//
-//
+/**
+ * @preserve
+ *
+ *                                      .,,,;;,'''..
+ *                                  .'','...     ..',,,.
+ *                                .,,,,,,',,',;;:;,.  .,l,
+ *                               .,',.     ...     ,;,   :l.
+ *                              ':;.    .'.:do;;.    .c   ol;'.
+ *       ';;'                   ;.;    ', .dkl';,    .c   :; .'.',::,,'''.
+ *      ',,;;;,.                ; .,'     .'''.    .'.   .d;''.''''.
+ *     .oxddl;::,,.             ',  .'''.   .... .'.   ,:;..
+ *      .'cOX0OOkdoc.            .,'.   .. .....     'lc.
+ *     .:;,,::co0XOko'              ....''..'.'''''''.
+ *     .dxk0KKdc:cdOXKl............. .. ..,c....
+ *      .',lxOOxl:'':xkl,',......'....    ,'.
+ *           .';:oo:...                        .
+ *                .cd,      ╔═╗┌┬┐┬┌┬┐┌─┐┬─┐    .
+ *                  .l;     ║╣  │││ │ │ │├┬┘    '
+ *                    'l.   ╚═╝─┴┘┴ ┴ └─┘┴└─   '.
+ *                     .o.                   ...
+ *                      .''''','.;:''.........
+ *                           .'  .l
+ *                          .:.   l'
+ *                         .:.    .l.
+ *                        .x:      :k;,.
+ *                        cxlc;    cdc,,;;.
+ *                       'l :..   .c  ,
+ *                       o.
+ *                      .,
+ *
+ *      ╦═╗┌─┐┌─┐┬  ┬┌┬┐┬ ┬  ╔═╗┌┬┐┬┌┬┐┌─┐┬─┐  ╔═╗┬─┐┌─┐ ┬┌─┐┌─┐┌┬┐
+ *      ╠╦╝├┤ ├─┤│  │ │ └┬┘  ║╣  │││ │ │ │├┬┘  ╠═╝├┬┘│ │ │├┤ │   │
+ *      ╩╚═└─┘┴ ┴┴─┘┴ ┴  ┴   ╚═╝─┴┘┴ ┴ └─┘┴└─  ╩  ┴└─└─┘└┘└─┘└─┘ ┴
+ *
+ *
+ * Created by Benjamin Reynolds on 7/14/16.
+ *
+ * Copyright (c) 2015 Valentin Heun
+ *
+ * All ascii characters above must be included in any redistribution.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 
 #include "ofxWKWebViewDelegate.h"
 
