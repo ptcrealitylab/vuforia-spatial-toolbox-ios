9470ab1e59c4b90abab6f1733a7c5701dd556c65
Ben Reynolds <breynolds@ptc.com>
2019-04-09 17:10:26 -0400

    fixes getScreenshot API to actually rescale the image when provided S,M,L parameter.

--- Reality Editor iOS/JavaScriptAPIHandler.mm
+++ Reality Editor iOS/JavaScriptAPIHandler.mm
@@ -115,9 +115,47 @@
 - (void)getScreenshot:(NSString *)size callback:(NSString *)callback
 {
     UIImage* cameraImage = [[ARManager sharedManager] getCameraPixelBuffer];
+    CGFloat imageWidth = cameraImage.size.width;
+    CGFloat imageHeight = cameraImage.size.height;
+    
+    if ([size isEqualToString:@"S"]) {
+        // rescale small => 1/4 size
+        cameraImage = [self resizeImage:cameraImage newSize:CGSizeMake(imageWidth * 0.25, imageHeight * 0.25)];
+    } else if ([size isEqualToString:@"M"]) {
+        // rescale medium => 1/2 size
+        cameraImage = [self resizeImage:cameraImage newSize:CGSizeMake(imageWidth * 0.50, imageHeight * 0.50)];
+    } else { //if ([size isEqualToString:@"L"]) {
+        // don't rescale
+    }
+    
     NSData *imageData = UIImageJPEGRepresentation(cameraImage, 1.0);
     NSString *encodedString = [NSString stringWithFormat:@"'%@'", [imageData base64EncodedStringWithOptions:NSDataBase64EncodingEndLineWithCarriageReturn]];
     [delegate callJavaScriptCallback:callback withArguments:@[encodedString]];
+}
+
+- (UIImage *)resizeImage:(UIImage*)image newSize:(CGSize)newSize {
+    CGRect newRect = CGRectIntegral(CGRectMake(0, 0, newSize.width, newSize.height));
+    CGImageRef imageRef = image.CGImage;
+    
+    UIGraphicsBeginImageContextWithOptions(newSize, NO, 0);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    // Set the quality level to use when rescaling
+    CGContextSetInterpolationQuality(context, kCGInterpolationHigh);
+    CGAffineTransform flipVertical = CGAffineTransformMake(1, 0, 0, -1, 0, newSize.height);
+    
+    CGContextConcatCTM(context, flipVertical);
+    // Draw into the context; this scales the image
+    CGContextDrawImage(context, newRect, imageRef);
+    
+    // Get the resized image from the context and a UIImage
+    CGImageRef newImageRef = CGBitmapContextCreateImage(context);
+    UIImage *newImage = [UIImage imageWithCGImage:newImageRef];
+    
+    CGImageRelease(newImageRef);
+    UIGraphicsEndImageContext();
+    
+    return newImage;
 }
 
 - (void)setPause
