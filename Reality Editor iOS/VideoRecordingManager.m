f1916bd0a4764233e76cdc6bca1c0a2e0148ed45
Ben Reynolds <breynolds@ptc.com>
2019-08-30 01:20:54 -0400

    updates video recording to use screen recording correctly and compress videos a lot

--- Reality Editor iOS/VideoRecordingManager.m
+++ Reality Editor iOS/VideoRecordingManager.m
@@ -12,6 +12,10 @@
 #define temporaryDirectory NSTemporaryDirectory()
 
 @implementation VideoRecordingManager
+//{
+//    BOOL isRecording;
+//    CFTimeInterval recordingStartTime;
+//}
 
 + (id)sharedManager
 {
@@ -31,17 +35,19 @@
     // generates a random filename and saves to temp file directory before uploading to server
     NSString *videoOutPath = [temporaryDirectory stringByAppendingPathComponent:[[NSString stringWithFormat:@"%u", arc4random() % 1000] stringByAppendingPathExtension:@"mp4"]];
     self.assetWriter = [AVAssetWriter assetWriterWithURL:[NSURL fileURLWithPath:videoOutPath] fileType:AVFileTypeMPEG4 error:&error];
+//    self.assetWriter.shouldOptimizeForNetworkUse = YES;
     
-    NSDictionary *compressionProperties = @{AVVideoProfileLevelKey         : AVVideoProfileLevelH264HighAutoLevel,
+    NSDictionary *compressionProperties = @{AVVideoProfileLevelKey         : AVVideoProfileLevelH264BaselineAutoLevel,
                                             AVVideoH264EntropyModeKey      : AVVideoH264EntropyModeCABAC,
-                                            AVVideoAverageBitRateKey       : @(1920 * 1080 * 11.4),
+//                                            AVVideoAverageBitRateKey       : @(1920 * 1080 * 11.4),
+                                            AVVideoAverageBitRateKey       : @(640 * 360 * 11.4),
                                             AVVideoMaxKeyFrameIntervalKey  : @60,
                                             AVVideoAllowFrameReorderingKey : @NO};
 
     NSDictionary *videoSettings = @{AVVideoCompressionPropertiesKey : compressionProperties,
                                     AVVideoCodecKey                 : AVVideoCodecTypeH264,
-                                    AVVideoWidthKey                 : @1920,
-                                    AVVideoHeightKey                : @1080};
+                                    AVVideoWidthKey                 : @640, //@1920,
+                                    AVVideoHeightKey                : @360}; //@1080};
 
     self.assetWriterInput = [AVAssetWriterInput assetWriterInputWithMediaType:AVMediaTypeVideo outputSettings:videoSettings];
 
@@ -49,21 +55,21 @@
     [self.assetWriterInput setMediaTimeScale:60];
     [self.assetWriter setMovieTimeScale:60];
     [self.assetWriterInput setExpectsMediaDataInRealTime:YES];
-
+    
     self.screenRecorder = [RPScreenRecorder sharedRecorder];
-
+    
     [self.screenRecorder startCaptureWithHandler:^(CMSampleBufferRef  _Nonnull sampleBuffer, RPSampleBufferType bufferType, NSError * _Nullable error) {
         if (CMSampleBufferDataIsReady(sampleBuffer)) {
             if (self.assetWriter.status == AVAssetWriterStatusUnknown) {
                 [self.assetWriter startWriting];
                 [self.assetWriter startSessionAtSourceTime:CMSampleBufferGetPresentationTimeStamp(sampleBuffer)];
             }
-
+            
             if (self.assetWriter.status == AVAssetWriterStatusFailed) {
                 NSLog(@"An error occured.");
                 return;
             }
-
+            
             if (bufferType == RPSampleBufferTypeVideo) {
                 if (self.assetWriterInput.isReadyForMoreMediaData) {
                     [self.assetWriterInput appendSampleBuffer:sampleBuffer];
@@ -81,6 +87,118 @@
     }];
 }
 
+/*
+- (void)startRecordingWithoutAR:(NSString *)objectKey ip:(NSString *)objectIP
+{
+    NSError *error = nil;
+
+    // generates a random filename and saves to temp file directory before uploading to server
+    NSString *videoOutPath = [temporaryDirectory stringByAppendingPathComponent:[[NSString stringWithFormat:@"%u", arc4random() % 1000] stringByAppendingPathExtension:@"mp4"]];
+    self.assetWriter = [AVAssetWriter assetWriterWithURL:[NSURL fileURLWithPath:videoOutPath] fileType:AVFileTypeMPEG4 error:&error];
+    self.assetWriter.shouldOptimizeForNetworkUse = YES;
+    
+    NSDictionary *compressionProperties = @{AVVideoProfileLevelKey         : AVVideoProfileLevelH264BaselineAutoLevel,
+                                            AVVideoH264EntropyModeKey      : AVVideoH264EntropyModeCABAC,
+                                            //                                            AVVideoAverageBitRateKey       : @(1920 * 1080 * 11.4),
+                                            AVVideoAverageBitRateKey       : @(640 * 360 * 11.4),
+                                            AVVideoMaxKeyFrameIntervalKey  : @60,
+                                            AVVideoAllowFrameReorderingKey : @NO};
+    
+    NSDictionary *videoSettings = @{AVVideoCompressionPropertiesKey : compressionProperties,
+                                    AVVideoCodecKey                 : AVVideoCodecTypeH264,
+                                    AVVideoWidthKey                 : @640, //@1920,
+                                    AVVideoHeightKey                : @360}; //@1080};
+    
+    self.assetWriterInput = [AVAssetWriterInput assetWriterInputWithMediaType:AVMediaTypeVideo outputSettings:videoSettings];
+    
+    [self.assetWriterInput setMediaTimeScale:60];
+    [self.assetWriter setMovieTimeScale:60];
+    [self.assetWriterInput setExpectsMediaDataInRealTime:YES];
+    
+//    NSDictionary *pixelBufferAttributes = @{kCVPixelBufferPixelFormatTypeKey : kCVPixelFormatType_32BGRA,
+//                                    kCVPixelBufferWidthKey                 : @640, //@1920,
+//                                    kCVPixelBufferHeightKey                : @360}; //@1080};
+    
+    NSDictionary *sourcePixelBufferAttributesDictionary = [NSDictionary dictionaryWithObjectsAndKeys:
+                                                           [NSNumber numberWithInt:kCVPixelFormatType_32ARGB], kCVPixelBufferPixelFormatTypeKey, nil];
+
+    
+    self.assetWriterPixelBufferInput = [AVAssetWriterInputPixelBufferAdaptor assetWriterInputPixelBufferAdaptorWithAssetWriterInput:self.assetWriterInput sourcePixelBufferAttributes:sourcePixelBufferAttributesDictionary];
+
+//AVAssetWriterInputPixelBufferAdaptor(assetWriterInput: assetWriterVideoInput,
+                                        //                               sourcePixelBufferAttributes: pixelBufferAttributes)
+    
+    [self.assetWriter addInput:self.assetWriterInput];
+    
+    recordingStartTime = CACurrentMediaTime();
+    isRecording = true;
+    [NSTimer scheduledTimerWithTimeInterval:1.0
+                                     target:self
+                                   selector:@selector(writeFrame)
+                                   userInfo:nil
+                                    repeats:NO];
+}
+
+- (void)writeFrame
+{
+    NSLog(@"writeFrame");
+    
+    UIImage* cameraBackground = [[ARManager sharedManager] getCameraPixelBuffer];
+    
+    if (!isRecording) {
+        return;
+    }
+    
+    // Create a dummy pixel buffer to try the encoding
+    // on something simple.
+    CVPixelBufferRef pixelBuffer = NULL;
+    CVReturn status = CVPixelBufferCreate(kCFAllocatorDefault, width, height,
+                                          kCVPixelFormatType_32BGRA, NULL, &pixelBuffer);
+    NSParameterAssert(status == kCVReturnSuccess && pixelBuffer != NULL);
+    
+//    [self.assetWriterInput appen]
+    
+//    while !assetWriterVideoInput.isReadyForMoreMediaData {}
+    
+//    guard let pixelBufferPool = assetWriterPixelBufferInput.pixelBufferPool else {
+//        print("Pixel buffer asset writer input did not have a pixel buffer pool available; cannot retrieve frame")
+//        return
+//    }
+    
+//    var maybePixelBuffer: CVPixelBuffer? = nil
+//    let status  = CVPixelBufferPoolCreatePixelBuffer(nil, pixelBufferPool, &maybePixelBuffer)
+//    if status != kCVReturnSuccess {
+//        print("Could not get pixel buffer from asset writer input; dropping frame...")
+//        return
+//    }
+//
+//    guard let pixelBuffer = maybePixelBuffer else { return }
+//
+//    CVPixelBufferLockBaseAddress(pixelBuffer, [])
+//    let pixelBufferBytes = CVPixelBufferGetBaseAddress(pixelBuffer)!
+//
+//
+//    // Use the bytes per row value from the pixel buffer since its stride may be rounded up to be 16-byte aligned
+//    let bytesPerRow = CVPixelBufferGetBytesPerRow(pixelBuffer)
+//    let region = MTLRegionMake2D(0, 0, texture.width, texture.height)
+//
+//    texture.getBytes(pixelBufferBytes, bytesPerRow: bytesPerRow, from: region, mipmapLevel: 0)
+//
+//
+    CFTimeInterval frameTime = CACurrentMediaTime() - recordingStartTime;
+//    let presentationTime = CMTimeMakeWithSeconds(frameTime, preferredTimescale: 240)
+//
+//    CVBufferSetAttachment(pixelBuffer, kCVImageBufferAlphaChannelModeKey, kCVImageBufferAlphaChannelMode_PremultipliedAlpha, .shouldPropagate)
+//
+//
+//    assetWriterPixelBufferInput.append(pixelBuffer, withPresentationTime: presentationTime)
+//
+//
+//
+//    CVPixelBufferUnlockBaseAddress(pixelBuffer, [])
+}
+*/
+
 - (void)stopRecording:(NSString *)videoId
 {
     [self.screenRecorder stopCaptureWithHandler:^(NSError * _Nullable error) {
@@ -95,6 +213,7 @@
                 self.assetWriterInput = nil;
                 self.assetWriter = nil;
                 self.screenRecorder = nil;
+//                isRecording = false;
                 
             }];
         }
