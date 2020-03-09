2b7e171faa01b974b5eaf88f79bb8404eae9f35f
James Hobin <jhobin@ptc.com>
2020-03-09 16:13:02 -0400

    Update setup.sh for new repo locations

--- setup.sh
+++ setup.sh
@@ -11,20 +11,32 @@ cd bin/data
 
 if ! [ -d "userinterface" ]; then
   echo "Cloning userinterface"
-  git clone https://github.com/PTCInc/RE-userinterface/ userinterface
+  git clone --shallow-since=2020-03-01 git@github.com:ptcrealitylab/vuforia-spatial-toolbox-userinterface.git userinterface
 fi
+
 cd userinterface
 git pull
 cd ..
 
 if ! [ -d "RE-server" ]; then
   echo "Cloning server"
-  git clone https://github.com/PTCInc/RE-server/
-  cd RE-server
-  git checkout miniServerForIos
-  cd ..
+  git clone git@github.com:ptcrealitylab/vuforia-spatial-edge-server.git
 fi
-cd RE-server
+
+cd vuforia-spatial-edge-server
 git pull
+if ! [ -d "addons" ]; then
+  echo "Cloning addons"
+  mkdir addons
+  cd addons
+  if ! [ -d "vuforia-spatial-core-addon" ]; then
+    git clone git@github.com:ptcrealitylab/vuforia-spatial-core-addon.git
+  fi
+  cd vuforia-spatial-core-addon
+  git pull
+  npm install
+  cd .. # core-addon
+  cd .. # addons
+fi
 npm install
 cd ..
