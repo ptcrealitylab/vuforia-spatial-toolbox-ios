a288ee8c08b0ffdfa3e028fa4cdd1507482583e8
Ben Reynolds <breynolds@ptc.com>
2020-04-03 16:17:13 -0400

    Adds secondary project to repository expressly for the purpose of debugging the UI on the simulator. loads the UI into a wkwebview without vuforia or any other ios libraries. doesnt respond to messages from the JS<>iOS API.

---
+++ Toolbox-UI-Debugger/Toolbox-UI-Debugger/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  Toolbox-UI-Debugger
+//
+//  Created by Benjamin Reynolds on 4/3/20.
+//  Copyright © 2020 Reality Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
