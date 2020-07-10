e36a8919b9319fadf0afdf94c30fdc676c1189c1
Ben Reynolds <breynolds@ptc.com>
2020-07-10 14:16:53 -0400

    cleanup changes to UI Debugger project

--- Toolbox-UI-Debugger/Toolbox-UI-Debugger/REWebViewSimplified.h
+++ Toolbox-UI-Debugger/Toolbox-UI-Debugger/REWebViewSimplified.h
@@ -1,10 +1,12 @@
 //
-//  REWebView.h
+//  REWebViewSimplified.h
 //  Vuforia Spatial Toolbox
 //
 //  Created by Benjamin Reynolds on 7/2/18.
 //  Copyright © 2018 PTC. All rights reserved.
 //
+// This is a modified version of REWebView.h, which removes all references to the Node.js server
+// as that server is unnecessary for debugging layout of the app UI
 
 #import <UIKit/UIKit.h>
 #import <WebKit/WebKit.h>
