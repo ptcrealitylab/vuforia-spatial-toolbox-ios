7849a8a7e696721ff3445627c7a3c82b57d3b228
Ben Reynolds <breynolds@ptc.com>
2021-04-19 11:15:19 -0400

    applies commit to fix screenbuffer size to allow screenrecording on larger ios devices (iphone 11plus, ets)

--- Vuforia Spatial Toolbox/Vuforia Application/SampleAppRenderer.mm
+++ Vuforia Spatial Toolbox/Vuforia Application/SampleAppRenderer.mm
@@ -37,7 +37,8 @@
     #pragma mark - Spatial Toolbox Extensions to Vuforia Sample Application (private fields)
     BOOL isRecording;
     CGSize maxScreenSize; // if the screen is bigger than this, we clip it when writing to video
-    GLchar pixels[(1920) * (1080) * 4 + 1]; // allocates at least enough memory for the video, since we don't know the exact screen resolution until runtime. sized to fit the 1080p video. ensure maxScreenSize is set to the same dimensions. the +1 shifts the buffer from RGBA to ARGB. bad side effect is that alpha channel is shifted by 1 pixel, but because the alph channel is uniform it doesn't matter
+    // IMPORTANT: this needs to be manually set so that it fits the largest available screen size of any iOS device (currently iPhone 11 Pro is 2688x1242)
+    GLchar pixels[(2688) * (1242) * 4 + 1]; // allocates at least enough memory for the video, since we don't know the exact screen resolution until runtime. sized to fit the 1080p video. ensure maxScreenSize is set to the same dimensions. the +1 shifts the buffer from RGBA to ARGB. bad side effect is that alpha channel is shifted by 1 pixel, but because the alph channel is uniform it doesn't matter
     #pragma mark -
 }
 // SampleApplicationControl delegate (receives callbacks in response to particular
