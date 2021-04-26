0493ee51ba043a7a01faf49c960b0a7334ba0c68
James Hobin <jhobin@ptc.com>
2021-04-26 11:56:57 -0400

    Add implementation for properly handling notifications in the future

--- Vuforia Spatial Toolbox/AppDelegate.m
+++ Vuforia Spatial Toolbox/AppDelegate.m
@@ -98,6 +98,15 @@
     NSLog(@"didReceiveRemoteNotification: %@", userInfo);
 }
 
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center
+    didReceiveNotificationResponse:(nonnull UNNotificationResponse *)response
+    withCompletionHandler:(nonnull void (^)(void))completionHandler {
+    NSLog(@"didReceiveNotificationResponse: %@", response);
+    NSLog(@"the cooler notification response: %@", response.notification.request.content.userInfo);
+    // NSString* userId = response.notification.request.content.userInfo["userId"];
+    completionHandler();
+}
+
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
