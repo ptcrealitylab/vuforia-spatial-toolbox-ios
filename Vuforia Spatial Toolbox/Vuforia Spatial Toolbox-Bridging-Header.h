b681978de6090f29b4cb36b23c86ab45b2abebe9
benptc <breynolds@ptc.com>
2021-09-29 11:58:17 -0400

    update build settings and add bridging header for swift

---
+++ Vuforia Spatial Toolbox/Vuforia Spatial Toolbox-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Vuforia Spatial Toolbox-Bridging-Header.h
+//  Vuforia Spatial Toolbox
+//
+//  Created by Ben Reynolds on 9/29/21.
+//  Copyright © 2021 Reality Lab. All rights reserved.
+//
+
+#ifndef Vuforia_Spatial_Toolbox_Bridging_Header_h
+#define Vuforia_Spatial_Toolbox_Bridging_Header_h
+
+#import "VuforiaWrapper.h"
+
+#endif /* Vuforia_Spatial_Toolbox_Bridging_Header_h */
